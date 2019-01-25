ca104664b34e5a0937478a473b7b2086ce709072
carpenter-js <carpenterbarrett@gmail.com>
2019-01-25 09:43:17 -0700

    started delete

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const catData = [{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}];
+
+const dogData = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}];
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +41,31 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.delete('api/cat', (req, res, next) => {
+  catData.delete(catData[0])
+  .then(data => {
+    if (data) {
+      res.json(data).sendStatus(204);
+    }
+  })
+  .catch(err => {
+    next(err);
+  });
+}
+
+
+app.delete('api/dog', (req, res, next) => {
+  dogData.delete(dogData[0])
+    .then(data => {
+      if (data) {
+        res.json(data).sendStatus(204);
+      }
+    })
+    .catch(err => {
+      next(err);
+});
+}
 
 function runServer(port = PORT) {
   const server = app
