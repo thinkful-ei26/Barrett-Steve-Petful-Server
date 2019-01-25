631b629ee32375ff21b08966a69595603bed929d
carpenter-js <carpenterbarrett@gmail.com>
2019-01-25 09:53:42 -0700

    working on delete

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const { PORT, CLIENT_ORIGIN } = require('./config')
-const { dbConnect } = require('./db-mongoose')
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
-const app = express()
+const app = express();
 
 let dogs = [
   {
@@ -22,7 +22,7 @@ let dogs = [
     breed: 'Golden Retriever',
     story: 'Owner Passed away'
   }
-]
+];
 let cats = [
   {
     imageURL:
@@ -35,62 +35,43 @@ let cats = [
     breed: 'Bengal',
     story: 'Thrown on the street'
   }
-]
-app.get('/api/cat', function(req, res) {
-  res.send(cats[0])
-})
-
-app.get('/api/dog', function(req, res) {
-  res.send(dogs[0])
-})
-
-const catData = [{
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-}];
-
-const dogData = [{
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
-}];
+];
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
-)
+);
 
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
-)
+);
 
-app.delete('api/cat', (req, res, next) => {
-  catData.delete(catData[0])
-  .then(data => {
-    if (data) {
-      res.json(data).sendStatus(204);
-    }
-  })
-  .catch(err => {
-    next(err);
-  });
-}
+app.get('/api/cat', function(req, res) {
+  res.send(cats[0]);
+});
+
+app.get('/api/dog', function(req, res) {
+  res.send(dogs[0]);
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  cats.delete()
+    .then((data) => {
+      if (data) {
+        res.json(data).sendStatus(204);
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 
-app.delete('api/dog', (req, res, next) => {
-  dogData.delete(dogData[0])
+app.delete('/api/dog', (req, res, next) => {
+  dogs.delete(dogs[0])
     .then(data => {
       if (data) {
         res.json(data).sendStatus(204);
@@ -98,23 +79,23 @@ app.delete('api/dog', (req, res, next) => {
     })
     .catch(err => {
       next(err);
+    });
 });
-}
 
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`)
+      console.info(`App listening on port ${server.address().port}`);
     })
     .on('error', err => {
-      console.error('Express failed to start')
-      console.error(err)
-    })
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 if (require.main === module) {
-  dbConnect()
-  runServer()
+  dbConnect();
+  runServer();
 }
 
-module.exports = { app }
+module.exports = { app };
